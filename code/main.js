42f3dbc6c18287df7e324e55ab9bdeeaa9bc2c30
dgleyramos1 <dgleysilva@gmail.com>
2021-05-20 09:58:40 -0300

    adicionado alert com JQuery para avisar o recebimento do formulário

---
+++ code/main.js
@@ -0,0 +1,5 @@
+//Aguarda o DOM estar pronto para executar o script JQuery
+$(documente).ready(function(){
+    // Recebe o evento submit do formulário
+    $("#contactForm").submit(function(event){
+      }
